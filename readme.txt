5bcd16d7b0fc2a12a4230c8681aa9e0b73fc342b
Dipak <sipu.dipak@gmail.com>
2020-07-01 22:07:19 +0530

    1st Commit

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Products Coming Soon ===
+Contributors: dipakbbsr
+Donate link: https://www.paypal.me/dipakpusti
+Tags: comments, spam
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Enable nice coming soon templates for your WooCommerce products when they are out of stock temporarily. 
+
+== Description ==
+
+Do you feel something is left out from this plugin? Let me know your ideas or suggestion, I'll add them as soon as possible. Feel free to mail me at, [sipu.dipak@gmail.com](mailto:sipu.dipak@gmail.com)
+
+== Installation ==
+
+1. Upload the plugin to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Follow the settings page to configure the plugin.
+
+== Changelog ==
+
+= 0.0.1 =
+* Initial Release.
+
+== Upgrade Notice ==
+
+= 0.0.1 =
+New version is available. Please update to get awesome features.
---
+++ wc-products-coming-soon.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ *
+ * @link              http://aroundtheweb.in
+ * @since             1.0.0
+ * @package           Wc_Products_Coming_Soon
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Products Coming Soon
+ * Description:       Enable nice coming soon templates for your WooCommerce products when they are out of stock temporarily.
+ * Version:           1.0.0
+ * Author:            Dipak Kumar Pusti
+ * Author URI:        https://profiles.wordpress.org/dipakbbsr/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wc-pcs
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'WCP_CS_VERSION', '1.0.0' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-wc-products-coming-soon.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function wc_pcs_initialize() {
+	$plugin = new Wc_Products_Coming_Soon();
+	$plugin->run();
+}
+wc_pcs_initialize();
